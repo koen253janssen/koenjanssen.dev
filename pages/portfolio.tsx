526a60be68e6888d47944fdb7a73a718d3fb306e
Koen Janssen <mail@koenjanssen.xyz>
2022-04-26 11:29:19 +0200

    Converted Pages to Typescript

---
+++ pages/portfolio.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Container, Heading } from '@chakra-ui/react';
+import Paragraph from '../components/paragraph';
+
+export default class Portfolio extends React.Component {
+    public render() {
+        return (
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Portfolio
+                </Heading>
+
+                <Paragraph>Coming soon...</Paragraph>
+            </Container>
+        );
+    }
+}
